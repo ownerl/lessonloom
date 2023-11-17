7952eff4112414401377c3c6e37854fdfb38abf0
Abay <ibrayev.abay@gmail.com>
2023-11-17 10:43:18 -0700

    mongo url

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,9 +2,7 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
-import AuthPage from '../Auth/AuthPage';
 import NewOrderPage from '../NewOrder/NewOrderPage';
-import OrderHistoryPage from '../OrderHistory/OrderHistoryPage';
 import NavBar from '../../components/Nav/NavBar';
 
 export default function App() {
