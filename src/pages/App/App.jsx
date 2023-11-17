198f74d2078b678857c16897843bae94b36dd775
Abay <ibrayev.abay@gmail.com>
2023-11-16 17:28:22 -0700

    video embed

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -11,17 +11,7 @@ export default function App() {
   const [user, setUser] = useState(getUser());
   return (
     <main className="App">
-      { user ?
-        <>
-          <NavBar user={user} setUser={setUser}/>
-          <Routes>
-            <Route path="/orders/new" element={ <NewOrderPage />}/>
-            <Route path="/orders" element={ <OrderHistoryPage /> }/>
-          </Routes>
-        </>
-        :
-        <AuthPage setUser={setUser}/>
-      }
+          <NewOrderPage />
     </main>
   );
 }
--- src/pages/NewOrder/NewOrderPage.jsx
+++ src/pages/NewOrder/NewOrderPage.jsx
@@ -1,5 +1,55 @@
+import { useRef, useState } from "react";
+import YouTube from 'react-youtube';
+
 export default function NewOrderPage() {
+    const [videoUrl, setVideoUrl] = useState("");
+    const [isPlaying, setIsPlaying] = useState(true)
+    const playerRef = useRef(null);
+
+    let videoCode;
+    if (videoUrl) {
+        videoCode = videoUrl.split("v=")[1].split("&")[0];
+    }
+    const opts = {
+        height: "400",
+        width: "640",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 0,
+        },
+    };
+
+    const togglePlayPause = () => {
+        if (playerRef.current) {
+            isPlaying ? playerRef.current.pauseVideo() : playerRef.current.playVideo();
+            setIsPlaying(!isPlaying);
+        }
+    }
+
+    function testFunction() {
+        console.log('pause func works')
+    }
     return (
-        <h1>NewOrderPage</h1>
-    )
+        <main>
+            <h1>NewOrderPage</h1>
+            <p>https://www.youtube.com/watch?v=kkSf95iI984</p>
+            <p>https://www.youtube.com/watch?v=PkkFHO0kxPw</p>
+            <p>https://www.youtube.com/embed/PkkFHO0kxPw?si=pXBsZqgSDhpuJMbz</p>
+            <YouTube 
+                videoId={videoCode} 
+                opts={opts}
+                onPause={() => console.log('Paused!')}
+                onPlay={() => console.log('Playing!')}
+                onReady={(e) => playerRef.current = e.target}
+                />
+            <div className="form-container">
+                <label>YouTube Link</label>
+                <input
+                    value={videoUrl}
+                    onChange={(e) => setVideoUrl(e.target.value)}
+                />
+                <button onClick={togglePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+            </div>
+        </main>
+    );
 }
