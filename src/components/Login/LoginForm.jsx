b49ab47d1e127cb0a54dcb4d4dc6c6239daf4337
Abay <ibrayev.abay@gmail.com>
2023-11-18 22:07:53 -0700

    rm

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,7 +1,4 @@
 export default function Login () {
-    const google = () => {
-        window.open("http://localhost:5000/auth/google", "_self");
-    };
     return (
         <div className="login">
 
