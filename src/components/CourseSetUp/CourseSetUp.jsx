d42cdb6df4cffd7b4de58ca0ff5e3de322d1b583
mselbekk11 <mselbekk11@gmail.com>
2023-11-21 20:44:34 -0800

    got course info going to database

--- src/components/CourseSetUp/CourseSetUp.jsx
+++ src/components/CourseSetUp/CourseSetUp.jsx
@@ -3,8 +3,24 @@ import { useNavigate } from "react-router-dom";
 import * as course from "../../utilities/courses-api";
 import './CourseSetUp.css'
 import photoUpload from '../../img/gallery 1.png'
+import Select from 'react-select'
 
 export default function CourseSetUp() {
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = [
+    { value: 'Photography', label: 'Photography' },
+    { value: 'JavaScript', label: 'JavaScript' },
+    { value: 'Nutrition', label: 'Nutrition' },
+    { value: 'Physics', label: 'Physics' },
+    { value: 'Painting', label: 'Painting' },
+    { value: 'AI', label: 'AI' },
+    { value: 'Busienss', label: 'Busienss' },
+    { value: 'Yoga', label: 'Yoga' },
+    { value: 'Cooking', label: 'Cooking' },
+    { value: 'marketing', label: 'marketing' }
+  ]
 
   const [postImage, setPostImage] = useState("")
 
@@ -18,6 +34,7 @@ export default function CourseSetUp() {
     title: "",
     description: "",
     bannerImage: "",
+    categories: "",
 });
 
 const [error, setError] = useState("");
@@ -33,6 +50,12 @@ async function handleImageChange(e) {
   console.log(e.target.name)
   // setFile(base64)
   // setFile(URL.createObjectURL(e.target.files[0]));
+}
+
+async function handleCatChange(e) {
+  setCourseInfo({ ...courseInfo, 'categories': e.value });
+  console.log(e.value)
+  console.log(courseInfo)
 }
 
 function convertToBase64(file) {
@@ -51,6 +74,8 @@ function convertToBase64(file) {
 function handleChange(evt) {
   setCourseInfo({ ...courseInfo, [evt.target.name]: evt.target.value });
   setError("");
+  console.log(evt.target.name)
+  console.log(evt.target.value)
 }
 
 async function handleSubmit(evt) {
@@ -97,19 +122,16 @@ async function handleSubmit(evt) {
       />
     </div>
     <div className="bottom-row">
-      <h3>Choose up to 3 categories</h3>
-      <div className="categories">
-                <div className="cat">Photography</div>
-                <div className="cat">JavaScript</div>
-                <div className="cat">Nutrition</div>
-                <div className="cat">Physics</div>
-                <div className="cat">Painting</div>
-                <div className="cat">AI</div>
-                <div className="cat">Busienss</div>
-                <div className="cat">Yoga</div>
-                <div className="cat">Cooking</div>
-                <div className="cat">Marketing</div>
-            </div>
+      <h3>Choose a Category</h3>
+      <Select 
+      options={options} 
+      // name="categories" 
+      required
+      defaultValue={selectedOption}
+      onChange={handleCatChange}
+      // value={courseInfo.categories}
+      // onChange={(evt) => {setCourseInfo({ ...courseInfo, categories: evt.target.value });}}
+      />
     </div>
     <button type="submit">Create Course</button>
     <p className="error-message">&nbsp;{error}</p>
