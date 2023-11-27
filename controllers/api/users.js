dd577bc5dd236c7c0890d95471caa5c9de909eac
Abay <ibrayev.abay@gmail.com>
2023-11-26 21:18:23 -0700

    :D

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -5,28 +5,30 @@ module.exports = {
 };
 
 async function login(req, res) {
-    const userInfo = req.body.userInfo;
-    console.log("this is req info from login route: ", req.body);
-    console.log("dis da client id: ", req.body.CLIENT_ID);
-    console.log("dis da token id: ", req.body.idToken);
-    try {
-        // A user has logged in with OAuth...
-        console.log('before find user')
-        let user = await User.findOne({ googleId: userInfo.googleId });
-        // Existing user found, so provide it to passport
-        console.log('after find user: ', user)
-        if (user) return user;
-        console.log('after if return')
-        const newUser = await User.create({
-            name: userInfo.name,
-            email: userInfo.email,
-            picture: userInfo.picture,
-            googleId: userInfo.sub,
-        });
-        console.log('the user after awaits: ', newUser)
-        res.json(newUser);
-    } catch (err) {
-        res.json(err);
+    if (req.body) {
+        const userInfo = req.body.userInfo;
+        console.log("this is req info from login route: ", req.body);
+        console.log("dis da client id: ", req.body.CLIENT_ID);
+        console.log("dis da token id: ", req.body.idToken);
+        try {
+            // A user has logged in with OAuth...
+            console.log('before find user')
+            let user = await User.findOne({ googleId: userInfo.googleId });
+            // Existing user found, so provide it to passport
+            console.log('after find user: ', user)
+            if (user) return user;
+            console.log('after if return')
+            const newUser = await User.create({
+                name: userInfo.name,
+                email: userInfo.email,
+                picture: userInfo.picture,
+                googleId: userInfo.sub,
+            });
+            console.log('the user after awaits: ', newUser)
+            res.json(newUser);
+        } catch (err) {
+            res.json(err);
+        }
     }
 }
 
