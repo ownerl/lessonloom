b233a15319e70e35b56ec95894813c5945e59c1c
carldamey <carldamey@gmail.com>
2023-11-28 14:46:19 -0800

    basic lesson view functionality

--- src/pages/LessonViewPage/LessonViewPage.jsx
+++ src/pages/LessonViewPage/LessonViewPage.jsx
@@ -1,11 +1,21 @@
 import {useLocation} from "react-router-dom"
-import { useRef } from "react"
+import {useRef} from "react"
 import YouTube from "react-youtube"
 export default function LessonViewPage({state}) {
 	const location = useLocation()
 	const lessonInfo = location.state?.lessonInfo
 
+	const videoUrl = lessonInfo.youTubeLink
 	const playerRef = useRef(null)
+
+	let videoCode
+	try {
+		if (videoUrl) {
+			videoCode = videoUrl.split("v=")[1].split("&")[0]
+		}
+	} catch (err) {
+		console.log(err)
+	}
 	const opts = {
 		height: "230",
 		width: "487",
@@ -19,53 +29,25 @@ export default function LessonViewPage({state}) {
 	return (
 		<div>
 			<h1>course title & link here</h1>
-			<h1>{lessonInfo.title}</h1>
-			<h2>video here</h2>
-
-
-
+			<h1>LESSON (INSERT): {lessonInfo.title}</h1>
 
 			<div className="youtube">
-                        {videoCode ? (
-                            <YouTube
-                                videoId={videoCode}
-                                opts={opts}
-                                onPause={() => console.log("Paused!")}
-                                onPlay={() => console.log("Playing!")}
-                                onReady={(e) => (playerRef.current = e.target)}
-                            />
-                        ) : (
-                            <div></div>
-                        )}
-                    </div>
-                    <div className="form-container">
-                        {/* <label>YouTube Link</label>
-                <p>https://www.youtube.com/watch?v=kkSf95iI984</p> */}
-                        <input
-                            className="url"
-                            name="youTubeLink"
-                            placeholder="https://www.youtube.com/watch?v=kkSf95iI984"
-                            value={videoUrl}
-                            onChange={(evt) => {
-                                setVideoUrl(evt.target.value);
-                                setLessonInfo({ ...lessonInfo, [evt.target.name]: evt.target.value });
-                            }}
-                        />
-                        <button
-                            className="youtube-btn"
-                            onClick={togglePlayPause}
-                        >
-                            {isPlaying ? "Pause" : "Play"}
-                        </button>
-                    </div>
-
-
-
-
-
+				{videoCode ? (
+					<YouTube
+						videoId={videoCode}
+						opts={opts}
+						onPause={() => console.log("Paused!")}
+						onPlay={() => console.log("Playing!")}
+						onReady={(e) => (playerRef.current = e.target)}
+					/>
+				) : (
+					<div></div>
+				)}
+			</div>
 
 			<h2>{lessonInfo.description}</h2>
-			<h2>{JSON.stringify(lessonInfo)}</h2>
+			<h2>Post Lesson Task: {lessonInfo.task}</h2>
+			<h2>Notes: {lessonInfo.notes}</h2>
 		</div>
 	)
 }
