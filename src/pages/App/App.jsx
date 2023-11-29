0713d50188cb96d5df0bb8ab0717df5322961a2b
Abay <ibrayev.abay@gmail.com>
2023-11-28 23:36:14 -0700

    nov 28 sign off

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,52 +1,63 @@
-import {useState} from "react"
-import React from "react"
-import {Routes, Route, Navigate} from "react-router-dom"
-import {jwtDecode} from "jwt-decode"
-import NavBar from "../../components/Nav/NavBar"
-import CoursesIndexPage from "../CoursesIndex/CoursesIndexPage"
-import CoursePage from "../CoursePage/CoursePage"
-import CourseView from "../../components/CourseView/CourseView"
+import { useState } from "react";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "../../components/Nav/NavBar";
+import CoursesIndexPage from "../CoursesIndex/CoursesIndexPage";
+import CoursePage from "../CoursePage/CoursePage";
+import CourseView from "../../components/CourseView/CourseView";
 // import Lesson from "../../components/Lesson/LessonCard";
-import CreateCoursePage from "../CreateCoursePage/CreateCoursePage"
-import UserProfilePage from "../UserProfilePage/UserProfilePage"
-import UserFavoritesPage from "../UserFavoritesPage/UserFavoritesPage"
-import LessonViewPage from "../LessonViewPage/LessonViewPage"
-import "./App.css"
-import CourseEdit from "../../components/CourseEdit/CourseEdit"
-/*global google*/
+import CreateCoursePage from "../CreateCoursePage/CreateCoursePage";
+import UserProfilePage from "../UserProfilePage/UserProfilePage";
+import UserFavoritesPage from "../UserFavoritesPage/UserFavoritesPage";
+import LessonViewPage from "../LessonViewPage/LessonViewPage";
+import "./App.css";/*global google*/
 
-export const UserContext = React.createContext(null)
+export const UserContext = React.createContext(null);
 
 export default function App() {
-	const [user, setUser] = useState(null)
-	// const name = user ? user.name : 'Guest';
-	return (
-		<div className="App">
-			{/* <h1>Hello {name}</h1> */}
-			<UserContext.Provider value={{user: user, setUser: setUser}}>
-				<NavBar user={user} setUser={setUser} />
+    const [user, setUser] = useState(null);
 
-				<Routes>
-					<Route path="/" element={<Navigate to="/courses" />} />
-					<Route path="/courses" element={<CoursesIndexPage />} />
-					<Route path="/courses/test" element={<CoursePage />} />
-					<Route path="/:courseId/view" element={<CourseView />} />
-					<Route path="/:courseId" element={<CoursePage />} />
-					<Route path="/courses/:id/view" element={<LessonViewPage />} />
-					<Route path="/courses/create" element={<CreateCoursePage />} />
+    return (
+        <div className="App">
+            {/* <h1>Hello {name}</h1> */}
+            <UserContext.Provider value={{ user: user, setUser: setUser }}>
+                <NavBar user={user} setUser={setUser} />
 
-					<Route path="/lessons/:id" element={<LessonViewPage />} />
-					{user && (
-						<>
-							<Route
-								path="/user"
-								element={<UserProfilePage user={user} setUser={setUser} />}
-							/>
-							<Route path="/user/favorites" element={<UserFavoritesPage />} />
-						</>
-					)}
-				</Routes>
-			</UserContext.Provider>
-		</div>
-	)
+                <Routes>
+                    <Route path="/" element={<Navigate to="/courses" />} />
+                    <Route path="/courses" element={<CoursesIndexPage />} />
+                    <Route path="/courses/test" element={<CoursePage />} />
+                    <Route path="/:courseId/view" element={<CourseView />} />
+                    <Route path="/:courseId" element={<CoursePage />} />
+                    <Route
+                        path="/courses/:id/view"
+                        element={<LessonViewPage />}
+                    />
+                    <Route
+                        path="/courses/create"
+                        element={<CreateCoursePage />}
+                    />
+
+                    <Route path="/lessons/:id" element={<LessonViewPage />} />
+                    {user && (
+                        <>
+                            <Route
+                                path="/user"
+                                element={
+                                    <UserProfilePage
+                                        user={user}
+                                        setUser={setUser}
+                                    />
+                                }
+                            />
+                            <Route
+                                path="/user/favorites"
+                                element={<UserFavoritesPage />}
+                            />
+                        </>
+                    )}
+                </Routes>
+            </UserContext.Provider>
+        </div>
+    );
 }
