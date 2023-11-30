2aac18a0f713a0f7c78b1ea84d271d91493c415b
Abay <ibrayev.abay@gmail.com>
2023-11-29 22:32:48 -0700

    profile icons fixed

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -13,9 +13,9 @@ export default function UserProfilePage({ user }) {
         "Cooking & Nutrition": "/cooking-icon.svg",
         Math: "/math-icon.svg",
         Art: "/art-icon.svg",
-        Language: "/business-icon.svg",
-        "Business & Marketing": "/fitness-icon.svg",
-        "Health & Fitness": "/language-icon.svg",
+        Language: "/language-icon.svg",
+        "Business & Marketing": "/business-icon.svg",
+        "Health & Fitness": "/fitness-icon.svg",
         DIY: "/img/diy-icon.svg",
         Other: "/img/other-icon.svg",
     };
